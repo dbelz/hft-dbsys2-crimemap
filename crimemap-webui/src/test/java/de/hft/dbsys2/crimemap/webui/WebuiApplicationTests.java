1c46a146b758f04a42e8b568ea44d5d17eaa2f53
Dani Belz <dani.belz@gmail.com>
2021-05-27 22:46:03 +0200

    Work in progress...

---
+++ crimemap-webui/src/test/java/de/hft/dbsys2/crimemap/webui/WebuiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hft.dbsys2.crimemap.webui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
