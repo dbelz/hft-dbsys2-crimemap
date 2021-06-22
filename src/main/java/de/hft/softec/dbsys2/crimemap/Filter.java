9de56e3b819e04c51c88332ac76862d0afdf4584
Dani Belz <dani.belz@gmail.com>
2021-06-23 00:21:45 +0200

    String getter to Filter entity added

--- src/main/java/de/hft/softec/dbsys2/crimemap/Filter.java
+++ src/main/java/de/hft/softec/dbsys2/crimemap/Filter.java
@@ -1,13 +1,14 @@
 package de.hft.softec.dbsys2.crimemap;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
 public class Filter {
     
-    private long offense;
-    private long district;
+    private long offense = 0;
+    private long district = 0;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateStart;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
@@ -21,12 +22,26 @@ public class Filter {
         return this.offense;
     }
 
+    public String getOffenseString() {
+        if (offense == 0) {
+            return "";
+        }
+        return "" + offense;
+    }
+
     public void setDistrict(long id) {
         this.district = id;
     }
 
     public long getDistrict() {
         return this.district;
+    }
+
+    public String getDistrictString() {
+        if (district == 0) {
+            return "";
+        }
+        return "" + district;
     }
 
     public void setDateStart(Date dateStart) {
@@ -37,12 +52,26 @@ public class Filter {
         return dateStart;
     }
 
+    public String getDateStartString() {
+        if (dateStart == null) {
+            return "";
+        }
+        return new SimpleDateFormat("yyyy-MM-dd").format(dateStart);
+    }
+
     public void setDateEnd(Date dateEnd) {
         this.dateEnd = dateEnd;
     }
 
     public Date getDateEnd() {
         return dateEnd;
+    }
+
+    public String getDateEndString() {
+        if (dateEnd == null) {
+            return "";
+        }
+        return new SimpleDateFormat("yyyy-MM-dd").format(dateEnd);
     }
 
 }
