55652f530347d7b7bccb3278d42da856769f6df6
Dani Belz <dani.belz@gmail.com>
2021-06-22 16:00:51 +0200

    basic dump added

---
+++ resources/dump.sql
@@ -0,0 +1,131 @@
+-- MySQL dump 10.13  Distrib 8.0.25, for Linux (x86_64)
+--
+-- Host: localhost    Database: crimemap
+-- ------------------------------------------------------
+-- Server version	8.0.25-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `crime`
+--
+
+DROP TABLE IF EXISTS `crime`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `crime` (
+  `id` bigint NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `date_of_crime` date DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `lat` decimal(8,6) DEFAULT NULL,
+  `lon` decimal(8,6) DEFAULT NULL,
+  `district_id` bigint DEFAULT NULL,
+  `offense_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKqdp6vsk7qwxuy9rsss50xvsmm` (`district_id`),
+  KEY `FK7532ul22bcdvia1w2agemivmq` (`offense_id`),
+  CONSTRAINT `FK7532ul22bcdvia1w2agemivmq` FOREIGN KEY (`offense_id`) REFERENCES `offense` (`id`),
+  CONSTRAINT `FKqdp6vsk7qwxuy9rsss50xvsmm` FOREIGN KEY (`district_id`) REFERENCES `district` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `crime`
+--
+
+LOCK TABLES `crime` WRITE;
+/*!40000 ALTER TABLE `crime` DISABLE KEYS */;
+INSERT INTO `crime` VALUES (209,NULL,'2021-06-10','Gewalttat',48.780373,9.180171,14,7),(210,NULL,'2021-06-17','Besoffen auf dem Wasen',48.796716,9.217787,1,8);
+/*!40000 ALTER TABLE `crime` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `district`
+--
+
+DROP TABLE IF EXISTS `district`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `district` (
+  `id` bigint NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `district`
+--
+
+LOCK TABLES `district` WRITE;
+/*!40000 ALTER TABLE `district` DISABLE KEYS */;
+INSERT INTO `district` VALUES (1,'Bad Cannstatt'),(2,'Birkach'),(3,'Botnang'),(4,'Degerloch'),(5,'Feuerbach'),(6,'Hedelfingen'),(7,'Möhringen'),(8,'Mühlhausen'),(9,'Münster'),(10,'Obertürkheim'),(11,'Plieningen'),(12,'Sillenbuch'),(13,'Stammheim'),(14,'Stuttgart‐Mitte'),(15,'Stuttgart‐Nord'),(16,'Stuttgart‐Ost'),(17,'Stuttgart‐Süd'),(18,'Stuttgart‐West'),(19,'Untertürkheim'),(20,'Vaihingen'),(21,'Wangen'),(22,'Weilimdorf'),(23,'Zuffenhausen'),(170,'Bad Cannstatt'),(171,'Birkach'),(172,'Botnang'),(173,'Degerloch'),(174,'Feuerbach'),(175,'Hedelfingen'),(176,'Möhringen'),(177,'Mühlhausen'),(178,'Münster'),(179,'Obertürkheim'),(180,'Plieningen'),(181,'Sillenbuch'),(182,'Stammheim'),(183,'Stuttgart‐Mitte'),(184,'Stuttgart‐Nord'),(185,'Stuttgart‐Ost'),(186,'Stuttgart‐Süd'),(187,'Stuttgart‐West'),(188,'Untertürkheim'),(189,'Vaihingen'),(190,'Wangen'),(191,'Weilimdorf'),(192,'Zuffenhausen');
+/*!40000 ALTER TABLE `district` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hibernate_sequence`
+--
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+LOCK TABLES `hibernate_sequence` WRITE;
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` VALUES (211);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `offense`
+--
+
+DROP TABLE IF EXISTS `offense`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `offense` (
+  `id` bigint NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `offense`
+--
+
+LOCK TABLES `offense` WRITE;
+/*!40000 ALTER TABLE `offense` DISABLE KEYS */;
+INSERT INTO `offense` VALUES (1,'Burglary'),(2,'Accident'),(3,'Property crime'),(4,'Sex crime'),(5,'Drugs'),(6,'Fire'),(7,'Violent act'),(8,'Drunkenness'),(9,'Damage to property'),(161,'Burglary'),(162,'Accident'),(163,'Property crime'),(164,'Sex crime'),(165,'Drugs'),(166,'Fire'),(167,'Violent act'),(168,'Drunkenness'),(169,'Damage to property');
+/*!40000 ALTER TABLE `offense` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-06-22 14:43:25
